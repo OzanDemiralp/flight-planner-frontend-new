c55044495daa37c680b55158b053a30efbf0a6ad
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-28 14:33:48 +0300

    add date format util

---
+++ src/utils/datetime.js
@@ -0,0 +1,9 @@
+export const fmt = (iso) => {
+  const d = new Date(iso);
+  const yyyy = d.getUTCFullYear();
+  const mm = String(d.getUTCMonth() + 1).padStart(2, '0');
+  const dd = String(d.getUTCDate()).padStart(2, '0');
+  const hh = String(d.getUTCHours()).padStart(2, '0');
+  const min = String(d.getUTCMinutes()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd} ${hh}:${min} UTC`;
+};
