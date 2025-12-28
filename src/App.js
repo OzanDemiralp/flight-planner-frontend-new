58ecf5ada7b3a1c3d8ffa2edd494dcdd10077f14
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-28 17:34:45 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { Box, Container } from '@mui/material';
 import './App.css';
 import { trips } from './mock/trips';
 import TripList from './components/TripList/TripList.jsx';
+import Navbar from './components/layout/Navbar.jsx';
 
 function App() {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-      <Container maxWidth='md' sx={{ height: '100%', py: 2 }}>
-        <TripList trips={trips} />
-      </Container>
-    </Box>
+    <>
+      <Navbar />
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Container maxWidth='md' sx={{ height: '100%', py: 2 }}>
+          <TripList trips={trips} />
+        </Container>
+      </Box>
+    </>
   );
 }
 
