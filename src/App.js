fdf21fc5761003b28b6e401db5e31056f0f2df48
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-28 14:34:10 +0300

    display trip card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import TripCard from './components/TripCard/TripCard.jsx';
+import { trip } from './mock/trip.js';
 
 function App() {
-  return <div>Flight Planner Frontend 🚀</div>;
+  return (
+    <div>
+      <TripCard trip={trip} />{' '}
+    </div>
+  );
 }
 
 export default App;
