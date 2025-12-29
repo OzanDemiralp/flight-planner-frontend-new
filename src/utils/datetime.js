4038ce80c6f7f30f3183cefe5a675e89435dac47
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2025-12-29 16:00:24 +0300

    modify datetime util

--- src/utils/datetime.js
+++ src/utils/datetime.js
@@ -7,3 +7,20 @@ export const fmt = (iso) => {
   const min = String(d.getUTCMinutes()).padStart(2, '0');
   return `${yyyy}-${mm}-${dd} ${hh}:${min} UTC`;
 };
+
+export const todayYYYYMMDD = () => {
+  const d = new Date();
+  const yyyy = d.getFullYear();
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  const dd = String(d.getDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
+};
+
+export const plusDaysYYYYMMDD = (days) => {
+  const d = new Date();
+  d.setDate(d.getDate() + days);
+  const yyyy = d.getFullYear();
+  const mm = String(d.getMonth() + 1).padStart(2, '0');
+  const dd = String(d.getDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
+};
