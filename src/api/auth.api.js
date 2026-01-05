0323de6318ddb9ecb4beffdf5ab459a621a8a37c
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-01-05 17:11:16 +0300

    add api for auth endpoints

---
+++ src/api/auth.api.js
@@ -0,0 +1,9 @@
+import api from './client';
+
+export const register = (payload) => api.post('/auth/register', payload);
+
+export const login = (payload) => api.post('/auth/login', payload);
+
+export const logout = () => api.post('/auth/logout');
+
+export const me = () => api.get('/auth/me');
