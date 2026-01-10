7d0ea44c585ad170c75239db2b11a18f74eaa106
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-01-10 14:39:48 +0300

    add flash to screen

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
 import Navbar from './Navbar.jsx';
+import { useAuth } from '../../auth/authContext.jsx';
+import Flash from '../Flash.jsx';
 
 export default function Layout() {
+  const { flash, setFlash } = useAuth();
+
   return (
     <>
       <Navbar />
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
         <Container maxWidth='md' sx={{ height: '100%', py: 2 }}>
           <Outlet />
+          {flash && (
+            <Flash
+              open
+              message={flash.message}
+              severity={flash.severity}
+              onClose={() => setFlash(null)}
+            />
+          )}
         </Container>
       </Box>
     </>
