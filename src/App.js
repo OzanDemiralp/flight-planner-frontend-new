477c874966af7076954dfa1c3cef23d22933e386
Ozan Halis Demiralp <120143887+OzanDemiralp@users.noreply.github.com>
2026-01-10 14:29:52 +0300

    add protected routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,54 @@ import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/layout/Layout.jsx';
 
+import Home from './pages/Home.jsx';
 import SearchPage from './pages/SearchPage.jsx';
 import SavedPage from './pages/SavedTripsPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
+import RequireAuth from './auth/RequireAuth.jsx';
+import GuestOnly from './auth/GuestOnly.jsx';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route index element={<Navigate to='/search' replace />} />
-        <Route path='/search' element={<SearchPage />} />
-        <Route path='/saved' element={<SavedPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='*' element={<Navigate to='/search' replace />} />
+        <Route index element={<Home />} />
+
+        <Route
+          path='/search'
+          element={
+            <RequireAuth>
+              <SearchPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/saved'
+          element={
+            <RequireAuth>
+              <SavedPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path='/login'
+          element={
+            <GuestOnly>
+              <LoginPage />
+            </GuestOnly>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <GuestOnly>
+              <RegisterPage />
+            </GuestOnly>
+          }
+        />
+
+        <Route path='*' element={<Navigate to='/' replace />} />
       </Route>
     </Routes>
   );
